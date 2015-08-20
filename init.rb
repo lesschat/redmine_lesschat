bbae68f1bf81477e66fd7f7a791f69f9938b35f5
Yanlong Wang <avastms@ghostunix.org>
2015-08-20 15:42:20 +0800

    Update register info

--- init.rb
+++ init.rb
@@ -5,12 +5,12 @@ require_dependency 'redmine_lesschat/listener'
 Redmine::Plugin.register :redmine_lesschat do
 	name 'Redmine Lesschat'
 	author 'YC Tech Beijing'
-	url 'https://github.com/atinc/redmine-lesschat'
+	url 'https://github.com/lesschat/redmine_lesschat'
 	author_url 'https://www.lesschat.com'
 	description 'Lesschat integration'
 	version '0.1'
 
 	requires_redmine :version_or_higher => '0.8.0'
 
-	settings :default => {'webhook_url' => 'https://lesschat.com/'}, :partial => 'settings/lesschat_settings'
+	settings :default => {'webhook_url' => ''}, :partial => 'settings/lesschat_settings'
 end
